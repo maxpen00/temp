98a719d34d502ee7c1162e48e0033bb6c009d4c8
bony000 <134943277+incregeni@users.noreply.github.com>
2023-11-29 11:51:14 -0800

    chore: monitor loan wstETH amount

--- contracts/Strategy.sol
+++ contracts/Strategy.sol
@@ -101,6 +101,7 @@ contract Strategy {
     function loan(
         uint256 wstETHAmount
     ) public returns (uint256 repaidETHAmount) {
+        console.log("WstETHAmount : ", wstETHAmount);
         IAavePool(aavePool).flashLoanSimple(
             address(this),
             WstETH,
